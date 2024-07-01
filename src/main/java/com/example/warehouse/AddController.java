2689067bfe0c637d3e10ddd036c40308133319b5
Елена Писарева <pisarelena@yandex.ru>
2024-07-01 17:45:21 +0300

    изменения2

--- src/main/java/com/example/warehouse/AddController.java
+++ src/main/java/com/example/warehouse/AddController.java
@@ -1,7 +1,11 @@
 package com.example.warehouse;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 import java.sql.SQLException;
@@ -9,6 +13,10 @@ import java.sql.SQLException;
 public class AddController {
 
     Database database = new Database();
+
+    @FXML
+    private ComboBox<String> department;
+    ObservableList<String> program = FXCollections.observableArrayList("Отдел кадров", "Бухгалтерия", "Безопасности", "Продаж", "ИТ");
     @FXML
     public Button send;
 
@@ -16,19 +24,17 @@ public class AddController {
     private TextField data;
 
     @FXML
-    private TextField department;
-
-    @FXML
     private TextField name;
 
     @FXML
     void initialize()
     {
+        department.setItems(program);
 
     }
 
     public void sendData() throws SQLException {
-        database.singUpUser(name.getText(), data.getText(), Integer.valueOf(department.getText()));
+        database.singUpUser(name.getText(), data.getText(), department.getValue());
 
     }
 }
--- src/main/java/com/example/warehouse/Database.java
+++ src/main/java/com/example/warehouse/Database.java
@@ -2,6 +2,7 @@ package com.example.warehouse;
 
 import java.nio.file.FileSystems;
 import java.sql.*;
+import java.util.Objects;
 
 public class Database {
 
@@ -73,8 +74,25 @@ public class Database {
         }
         System.out.println("таблица создана");
     }
-    public void singUpUser(String name, String data, Integer department_id) throws SQLException {
-        PreparedStatement pstmt = dbConnection.prepareStatement("INSERT INTO employee (name, data, department_id) values ('"+name+"', '"+data+"', "+department_id+");");
+    public void singUpUser(String name, String data, String department_id) throws SQLException {
+        //String str = "SELECT id FROM department WHERE name = " +department_id;
+        //PreparedStatement stmt = dbConnection.prepareStatement(str);
+        //ResultSet resultSet = stmt.executeQuery();
+        //while (resultSet.next()) {
+            //System.out.println(resultSet.getInt(1));
+        //}
+       // PreparedStatement stmt = dbConnection.prepareStatement("SELECT id FROM department WHERE name IN ("+department_id+")");
+        //ResultSet resultSet = stmt.executeQuery();
+        int department = 0;
+        //System.out.println(department);
+        if(Objects.equals(department_id, "Отдел кадров")) department = 1;
+        if(Objects.equals(department_id, "Бухгалтерия")) department = 2;
+        if(Objects.equals(department_id, "Безопасности")) department = 3;
+        if(Objects.equals(department_id, "Продаж")) department = 4;
+        if(Objects.equals(department_id, "ИТ")) department = 5;
+        //System.out.println(department);
+
+        PreparedStatement pstmt = dbConnection.prepareStatement("INSERT INTO employee (name, data, department_id) values ('"+name+"', '"+data+"', '"+department+"');");
         pstmt.executeUpdate();
     }
 }
